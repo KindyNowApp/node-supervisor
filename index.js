8e7d01c91b584034b55e89f2cb302d902c69e1c0
Juan-Manuel Fluxà <jmfluxa@gmail.com>
2015-04-29 17:16:55 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+var nodemailer = require('nodemailer');
+// var pjson = require()
+
+var Supervisor = function(options) {
+
+  var emailFrom  = options.emailFrom;
+  var emailTo    = options.emailTo;
+  // var transporter = nodemailer.createTransport(options.transport);
+
+  var init = function() {
+
+    process.on('uncaughtException', function (err) {
+
+      console.log('supervisor => ' + err);
+
+      var mailOptions = {
+        from: emailFrom,
+        to: emailTo,
+        subject: 'Supervisor: ' + err.message,
+        text: err.stack
+      };
+
+    //   mailqueue.addPlain(err.stack, 'help@kindynow.com', err.message, function(err) {
+    //     if(err) {
+    //       console.log(err);
+    //     }
+    //     process.exit(1);
+    //   });
+    // });
+  }
+
+  /*
+   * Expose public API calls
+   */
+
+  this.init   = init;
+
+  return this;
+}
