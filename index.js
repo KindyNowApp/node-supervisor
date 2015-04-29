e5e9743e1d81c7299232423fbe5545260f187f3a
Juan-Manuel Fluxà <jmfluxa@gmail.com>
2015-04-29 18:10:50 +0200

    done

--- index.js
+++ index.js
@@ -1,39 +1,69 @@
 var nodemailer = require('nodemailer');
-// var pjson = require()
+var util = require('util');
+var pjson = require('../../package');
+
+
+module.exports = function(options) {
+
+  if (typeof options === 'object') {
+    return new Supervisor(options);
+  }
+  console.log('supervisor => initialization error, check options format');
+};
+
+/**
+ * Creates an object for exposing the Supervisor API
+ *
+ * @constructor
+ * @param {Object} options emailFrom, emailTo, transport object instance to pass the mails to
+ */
 
 var Supervisor = function(options) {
 
-  var emailFrom  = options.emailFrom;
-  var emailTo    = options.emailTo;
-  // var transporter = nodemailer.createTransport(options.transport);
+  this.emailFrom    = options.emailFrom;
+  this.emailTo      = options.emailTo;
+  this.appName      = pjson ? pjson.name : '';
+  this.appEnv       = process.env.NODE_ENV || ''
+  this.transporter  = nodemailer.createTransport(options.transport);
 
-  var _init = function() {
+}
 
-    process.on('uncaughtException', function (err) {
+Supervisor.prototype.init = function() {
 
-      console.log('supervisor => ' + err);
+  var _this = this;
 
-      var mailOptions = {
-        from: emailFrom,
-        to: emailTo,
-        subject: 'Supervisor: ' + err.message,
-        text: err.stack
-      };
+  console.log('supervisor => initialized');
 
-    //   mailqueue.addPlain(err.stack, 'help@kindynow.com', err.message, function(err) {
-    //     if(err) {
-    //       console.log(err);
-    //     }
-    //     process.exit(1);
-    //   });
-    // });
+  process.on('uncaughtException', function (err) {
+
+    console.log('supervisor => ' + err);
+
+    var mailOptions = {
+      from: _this.emailFrom,
+      to: _this.emailTo,
+      subject: util.format('[%s | %s] Supervisor: %s',_this.appName,_this.appEnv,err.message),
+      text: err.stack
+    };
+
+    _this.transporter.sendMail(mailOptions, function(err, info) {
+      if(err) {
+        console.log('supervisor => send email error : %s',err);
+      } else {
+        console.log('supervisor => %s',JSON.stringify(info));
+      }
+      process.exit(1);
     });
-  }
-  /*
-   * Expose public API calls
-   */
 
-  this.init   = _init;
+  });
+}
 
-  return this;
+Supervisor.prototype.test = function() {
+  this.transporter.sendMail({
+    from: this.emailFrom,
+    to: this.emailTo,
+    subject: util.format('[%s | %s] Supervisor: %s',this.appName,this.appEnv,'test email'),
+    text: 'This is a test email from supervisor'
+  }, function(err, info) {
+    console.log('supervisor => %s',err || info);
+  })
 };
